3a34ed1d77b02dfac626ac37e9346c6874f3504c
biddisco <biddisco@a8ef729d-7618-0410-a8c4-833010e87eec>
2009-09-24 12:07:23 +0000

    dsm tests added
    git-svn-id: https://svn.cscs.ch/csviz/trunk/Projects/ICARUS@1354 a8ef729d-7618-0410-a8c4-833010e87eec

---
+++ Testing/namepub_client.cpp
@@ -0,0 +1,71 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <string.h>
+#include <fstream>
+
+#define MAX_DATA 50
+
+char test[] = "tag=0 port=2564 description=agno ifname=148.187.130.32";
+
+int main( int argc, char *argv[] )
+{
+    int errs = 0;
+    char port_name_out[MPI_MAX_PORT_NAME];
+    char serv_name[256];
+    char buf[MAX_DATA];
+    int merr, mclass;
+    char errmsg[MPI_MAX_ERROR_STRING];
+    int msglen;
+    int rank, size;
+    MPI_Comm server;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if(size != 1) {
+      printf("Too many processes, only one accepted\n");
+      MPI_Finalize();
+      return 1;
+    }
+
+    strcpy(serv_name, "MyTest");
+    strcpy(buf, "Hello world!");
+
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+
+//    merr = MPI_Lookup_name(serv_name, MPI_INFO_NULL, port_name_out);
+    merr = 0;
+    if (merr) {
+      errs++;
+      MPI_Error_string(merr, errmsg, &msglen);
+      printf("Error in Lookup name: \"%s\"\n", errmsg);
+    }
+    else {
+//      strcpy( port_name_out, test );
+      char longbuff[256];
+      std::ifstream fserver("//ponte.cscs.ch/biddisco/agno_name.txt");
+      fserver.getline(longbuff, 256);
+      fserver.close();
+      strcpy(port_name_out, longbuff);
+
+      //
+      printf ("Our (client) port name is now %s \n", port_name_out);      
+      fflush(stdout);
+      merr = MPI_Comm_connect(port_name_out, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &server);
+      if (merr) {
+        errs++;
+        MPI_Error_string(merr, errmsg, &msglen);
+        printf("Error in Lookup name: \"%s\"\n", errmsg);
+      }
+    }
+
+    MPI_Send(buf, MAX_DATA, MPI_CHAR, 0, 1, server);
+
+
+    MPI_Comm_disconnect(&server);
+
+    MPI_Finalize();
+    return 0;
+}
---
+++ Testing/namepub_server.cpp
@@ -0,0 +1,87 @@
+#include "mpi.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fstream>
+
+#define MAX_DATA 50
+
+char test[] = "tag=0 port=1957 description=agno ifname=148.187.130.32";
+
+int main( int argc, char *argv[] )
+{
+    int errs = 0;
+    char port_name[MPI_MAX_PORT_NAME];
+    char serv_name[256];
+    int merr;
+    char errmsg[MPI_MAX_ERROR_STRING];
+    int msglen;
+    int rank, size;
+    char buf[MAX_DATA];
+    MPI_Status status;
+    MPI_Comm client;
+    MPI_Info info;
+
+    MPI_Init( &argc, &argv );
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if(size != 1) {
+      printf("Too many processes, only one accepted\n");
+      MPI_Finalize();
+      return 1;
+    }
+
+    std::ofstream server("//ponte.cscs.ch/biddisco/agno_name.txt");
+
+    strcpy(serv_name, "MyTest");
+
+    MPI_Comm_set_errhandler(MPI_COMM_WORLD, MPI_ERRORS_RETURN);
+
+    MPI_Info_create (&info);
+    MPI_Info_set( info, "ip_port", "1957");
+    MPI_Info_set( info, "ip_address", "148.187.130.32");
+      
+    strcpy(port_name, test);
+    MPI_Open_port(info, port_name);
+    printf ("Our port name is %s \n", port_name);      
+    server << port_name << std::endl;
+    server.close();
+
+    fflush(stdout);
+
+//    merr = MPI_Publish_name(serv_name, MPI_INFO_NULL, port_name);
+    merr = 0;
+    if (merr) {
+      errs++;
+      MPI_Error_string(merr, errmsg, &msglen);
+      printf("Error in Publish_name: \"%s\"\n", errmsg);
+    } 
+    else {
+      printf("Published port_name( %s )\n", port_name);
+    }
+
+    merr = MPI_Comm_accept(port_name, MPI_INFO_NULL, 0, MPI_COMM_WORLD, &client);
+    if (merr) {
+      errs++;
+      MPI_Error_string(merr, errmsg, &msglen);
+      printf("Error in MPI_Comm_accept : \"%s\"\n", errmsg);
+    } 
+
+    MPI_Recv(buf, MAX_DATA, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG, client, &status);
+
+    printf("Server received: %s\n", buf);
+
+    merr = MPI_Unpublish_name(serv_name, MPI_INFO_NULL, port_name);
+    if (merr) {
+      errs++;
+      MPI_Error_string(merr, errmsg, &msglen);
+      printf("Error in Unpublish name: \"%s\"\n", errmsg);
+    }
+
+    MPI_Close_port(port_name);
+    MPI_Finalize();
+    return EXIT_SUCCESS;
+}
+
