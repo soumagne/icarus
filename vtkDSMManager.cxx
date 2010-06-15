4b4d5da73949dbd3c46e58e450d1541d2cfe97dd
John Biddiscombe <biddisco@cscs.ch>
2010-06-15 11:06:27 +0200

    CMake setup changed after hdf5 build changes.
    Use H5FD_DSM_CONFIG_PATH and remove hard coded path

--- vtkDSMManager.cxx
+++ vtkDSMManager.cxx
@@ -488,13 +488,9 @@ bool vtkDSMManager::ReadDSMConfigFile()
 {
   H5FDdsmIniFile config;
   std::string configPath;
-  const char *dsm_env = getenv("DSM_CONFIG_PATH");
+  const char *dsm_env = getenv("H5FD_DSM_CONFIG_PATH");
   if (dsm_env) {
     configPath = std::string(dsm_env) + std::string("/.dsm_config");
-  }
-  else {
-    // Parse DSM configuration file
-    configPath = std::string(H5FDdsm_CONFIG_PATH) + std::string("/.dsm_config");
   }
   if (vtksys::SystemTools::FileExists(configPath.c_str())) {
     std::string mode = config.GetValue("DSM_COMM_SYSTEM", "Comm", configPath);
