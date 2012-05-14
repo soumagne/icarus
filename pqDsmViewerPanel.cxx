1350ed00b38ef329febae53cf47ed15d55d0fcd9
Jerome Soumagne <soumagne@cscs.ch>
2012-05-14 16:52:14 +0200

    Revert "Enable Partition Box export by default"
    This reverts commit 10fc5e5ada4f75f594d58ec260a8110a9e4472ba.

--- pqDsmViewerPanel.cxx
+++ pqDsmViewerPanel.cxx
@@ -973,10 +973,7 @@ void pqDsmViewerPanel::UpdateDsmPipeline()
       pqSMAdaptor::setProxyProperty(
           this->Internals->H5PartReader->GetProperty("DsmManager"), this->Internals->DsmProxy
       );
-      pqSMAdaptor::setElementProperty(
-        this->Internals->H5PartReader->GetProperty("ExportPartitionBoxes"), 1);
       this->Internals->H5PartReader->UpdateProperty("DsmManager");
-      this->Internals->H5PartReader->UpdateProperty("ExportPartitionBoxes");
     }
   }
 
